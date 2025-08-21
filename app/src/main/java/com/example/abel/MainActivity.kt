5ff1b58fa23eed21e5f8f1f7fa6d88834c2166b8
Muhammad Abel <muhammadabelabhinaya@mail.ugm.ac.id>
2025-08-22 05:45:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/abel/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.abel
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.abel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            txtNumber.text = number.toString()
+            btnCount.setOnClickListener {
+                number++
+                txtNumber.text = number.toString()
+            }
+            btnToast.setOnClickListener {
+                Toast.makeText(
+                    this@MainActivity, "Count $number",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
